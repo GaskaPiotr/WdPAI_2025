f9c5d6e3b5e999455b5732033b7e2b41feb8bb5a
GaskaPiotr <04piotr04@wp.pl>
2025-10-09 08:59:45 +0200

    added Routing class

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+include ("routing.php");
+
 echo "<h1>Hello world 💪</h1>";
 
 $path = trim($_SERVER['REQUEST_URI'], '/');
@@ -7,17 +9,6 @@ $path = parse_url($path, PHP_URL_PATH);
 
 //var_dump($path);
 
-
-switch ($path) {
-    case 'dashboard':
-        include 'public/views/dashboard.html';
-        break;
-    case 'login':
-        include 'public/views/login.html';
-        break;
-    default:
-        include 'public/views/404.html';
-        break;
-}
+Routing::route($path)
 
 ?>
