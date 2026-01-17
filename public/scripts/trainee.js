86cae160f31eab4e2659485508df39287d1e8538
GaskaPiotr <04piotr04@wp.pl>
2026-01-17 19:11:01 +0100

    Added option for trainer to see and update plan

--- public/scripts/trainee.js
+++ public/scripts/trainee.js
@@ -1,15 +1,15 @@
 function toggleNotifications() {
-            const dropdown = document.getElementById('notificationDropdown');
-            if (dropdown.style.display === 'none') {
-                dropdown.style.display = 'block';
-            } else {
-                dropdown.style.display = 'none';
-            }
-        }
+    const dropdown = document.getElementById('notificationDropdown');
+    if (dropdown.style.display === 'none') {
+        dropdown.style.display = 'block';
+    } else {
+        dropdown.style.display = 'none';
+    }
+}
 
-        // Zamknij jak klikniesz poza
-        window.onclick = function (event) {
-            if (!event.target.closest('.notifications-wrapper')) {
-                document.getElementById('notificationDropdown').style.display = 'none';
-            }
-        }
+// Zamknij jak klikniesz poza
+window.onclick = function (event) {
+    if (!event.target.closest('.notifications-wrapper')) {
+        document.getElementById('notificationDropdown').style.display = 'none';
+    }
+}
